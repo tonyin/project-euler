9f19e54500f2ef835debf02e2bba763a278f64e9
tony <tonyyin89@gmail.com>
2016-07-12 21:11:42 -0700

    p26 messy but works

--- p26.py
+++ p26.py
@@ -4,7 +4,53 @@ import time
 
 
 def main():
-    
+    max_n = 0
+    max_repeat = 0
+    for i in range(1, 1000):
+        mods = []
+        if i < 10:
+            j = 10
+            while j % i not in mods:
+                if j % i == 0: break
+                if j < i:
+                    j *= 10
+                    continue
+                mods.append(j%i)
+                j = (j - i*(j/i))*10
+            if mods and (j%i in mods):
+                repeat = len(mods) - mods.index(j%i)
+                if repeat > max_repeat:
+                    max_repeat = repeat
+                    max_n = i
+        if i >= 10 and i < 100:
+            j = 100
+            while j % i not in mods:
+                if j % i == 0: break
+                if j < i:
+                    j *= 10
+                    continue
+                mods.append(j%i)
+                j = (j - i*(j/i))*10
+            if mods and (j%i in mods):
+                repeat = len(mods) - mods.index(j%i)
+                if repeat > max_repeat:
+                    max_repeat = repeat
+                    max_n = i
+        if i >= 100 and i < 1000:
+            j = 1000
+            while j % i not in mods:
+                if j % i == 0: break
+                if j < i:
+                    j *= 10
+                    continue
+                mods.append(j%i)
+                j = (j - i*(j/i))*10
+            if mods and (j%i in mods):
+                repeat = len(mods) - mods.index(j%i)
+                if repeat > max_repeat:
+                    max_repeat = repeat
+                    max_n = i
+    print max_n
 
 if __name__ == '__main__':
 	start = time.time()
