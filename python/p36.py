4e82e638c2e2835a14d9d9b21d57f9fccc14aba9
Tony Yin <tonyyin89@gmail.com>
2017-01-29 21:23:56 -0800

    p36. rolled my own base 10 to 2 converter

---
+++ python/p36.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import time
+
+
+# Problem:
+# Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+def is_palindrone(n):
+    if n < 1: return False
+    elif n < 10: return True
+    elif len(str(n)) % 2 == 0:
+        for i in range(int((len(str(n))/2))):
+            if str(n)[i] != str(n)[-i-1]: return False
+        return True
+    else:
+        for i in range(int((len(str(n))-1)/2)):
+            if str(n)[i] != str(n)[-i-1]: return False
+        return True
+
+def base_10to2(n):
+    d = 0
+    while 2**(d+1) <= n:
+        d += 1
+    b2 = []
+    for i in sorted(range(d+1), reverse=True):
+        if 2**i <= n:
+            b2.append(1)
+            n = n - 2**i
+        else:
+            b2.append(0)
+    return int(''.join([str(i) for i in b2]))
+
+def main():
+    ub = 1000000 # upper bound
+    print(sum([i for i in range(ub) if is_palindrone(i) and is_palindrone(base_10to2(i))]))
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    print('Program runtime: {0:.3f}s'.format(time.time() - start))
